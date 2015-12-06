75aae267f6162c5f7ae6ad2779492caecba87ded
yindiao.sq <yindiao.sq@taobao.com>
2015-12-06 08:50:38 +0800

    init.....

--- upload/include/config.db.php
+++ upload/include/config.db.php
@@ -2,7 +2,7 @@
 defined('_VALID') or die('Restricted Access!');
 $config['db_type'] = 'mysql';
 $config['db_host'] = 'localhost';
-$config['db_user'] = '';
-$config['db_pass'] = '';
-$config['db_name'] = '';
+$config['db_user'] = 'root';
+$config['db_pass'] = 'ngsoqia';
+$config['db_name'] = 'avsdb';
 ?>
